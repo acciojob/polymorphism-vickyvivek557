2eb97fa05b8aa8ce98d9650b199665eec1d8972a
Vivek Kumar <vickyvivek557@gmail.com>
2023-08-10 12:33:58 +0530

    commiting

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,9 +2,7 @@ package com.driver;
 
 public  class Main {
     public static class Product{
-        Product(){
 
-        }
         public int product(int x, int y){
             return x * y;
         }
